099e721f567dfbb975a1277d18dc612be6735acc
Sergilho <michesergio@gmail.com>
2022-09-21 09:58:17 -0300

    mudando a lógica com javascript

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
-const inputSenha = document.getElementById('inputSenha');
-const lenInput = document.getElementById('inputRange');
-const infoLen = document.getElementById('label[for="labelLenghtId"]');
-const btnGerar = document.getElementById('btn');
+const senha = document.querySelector("#inputSenha");
+const slider = document.querySelector("#inputSlider");
+const sliderLabel = document.querySelector("#inputLabel");
+const btnGerar = document.querySelector("#btnGerar");
+const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*()+=0123456789";
+const novaSenha = "";
 
-const checkLetraMinus = document.getElementById('checkLetraMinus');
-const checkLetraMaius = document.getElementById('checkLetraMaius');
-const checkNumeros = document.getElementById('checkNumeros');
-const checkSimbolos = document.getElementById('checkSimbolos');
+sliderLabel.innerHTML = slider.value
+slider.addEventListener("change", () =>{
+    sliderLabel.innerHTML = slider.value
+})
 
-const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const simbolos = ["!", "@", "#", "$", "%", "&"];
-const letrasMinus = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"];
-const letrasMaius = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z"];
 
-infoLen.innerHTML = lenInput.value;
-lenInput.addEventListener("change", () => {
-    infoLen.innerHTML = lenInput.value;
+// function GerarSenha(){
+//     let senha1 = "";
+    
+//     for(let x = 0, i = caracteres.length; x < slider.length; x++){
+//         senha1 += caracteres.charAt(Math.floor(Math.random() * i));
+//     }
+  
+//     senha.innerHTML = senha1
+//     console.log(senha1)
+// }
+btnGerar.addEventListener("click", () =>{
+        let senha1 = "";
+    
+        for(let x = 0, i = caracteres.length; x < slider.length; x++){
+        senha1 += caracteres.charAt(Math.floor(Math.random() * i));
+        }
+  
+    senha.innerHTML = senha1
+    console.log(senha1)
 })
