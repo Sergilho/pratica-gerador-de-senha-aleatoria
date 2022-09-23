e58c5b39b20de3068927cce11e929f6417a14b24
Sergilho <michesergio@gmail.com>
2022-09-23 09:44:54 -0300

    Revisão final do projeto com pequenas modificações

--- js/script.js
+++ js/script.js
@@ -1,28 +1,17 @@
+
 let senha = document.querySelector("#inputSenha");
 let slider = document.querySelector("#inputSlider");
 let sliderLabel = document.querySelector("#inputLabel");
 let btnGerar = document.querySelector("#btnGerar");
 
+
+    /*Interatividade da label com o slider*/
 sliderLabel.innerHTML = slider.value
 slider.addEventListener("change", () =>{
     sliderLabel.innerHTML = slider.value
 })
 
-
-// function GerarSenha(){
-//     let caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*()+=0123456789";
-//     let senha1 = "";
-
-//     for(let x = 0; x < slider.value; x++){
-//         senha1 += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-//     }
-
-//     senha.classList.remove("hide")
-//     senha.innerHTML = senha1;
-    
-// }
-
-
+    /* Função que é ativada com o botão ao clicar */
 btnGerar.addEventListener("click", () =>{
     let caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*()+=0123456789";
     let senha1 = "";
